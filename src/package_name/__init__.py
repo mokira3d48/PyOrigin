5f22af9e45b42e10508eae40de7d2ee5b9e4933a
Doctor Mokira <arnoldmokira3d48@gmail.com>
2026-02-02 23:11:28 +0100

    Initialize logging and DEBUG settings
    Added logging setup and DEBUG configuration.

--- src/package_name/__init__.py
+++ src/package_name/__init__.py
@@ -1,5 +1,19 @@
+import os
+import logging
 from pathlib import Path
 from dotenv import load_dotenv
+
+DEBUG = ast.literal_eval(os.getenv('DEBUG', 'False'))
+
+# Set up logging:
+logging.basicConfig(
+    level=(logging.INFO if not DEBUG else logging.DEBUG),
+    format='%(levelname)s [%(asctime)s] \t %(message)s',
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler("pyorigin.log"),
+    ]
+)
 
 # loads the configs from .env
 load_dotenv()
@@ -7,3 +21,5 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = Path(__file__).resolve().parent
+
+__version__ = '0.1.0'
